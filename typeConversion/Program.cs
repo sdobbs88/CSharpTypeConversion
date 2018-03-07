b5ec8768c108bf20f10f3251e84943602a5013e2
Shaun <Shaun@HPLAPTOP>
2018-03-07 14:01:30 -0500

    Add project files.

---
+++ typeConversion/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace typeConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //byte b = 1;
+            //int i = b;
+            //Console.WriteLine(i);
+
+            try
+            {
+                string str = "true";
+                bool b = Convert.ToBoolean(str);
+                Console.WriteLine(b);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("The number could not be converted to a byte");
+            }
+        }
+    }
+}
